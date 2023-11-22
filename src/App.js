96f97513ab1f8b8e4bcb07b864e6d5914479fec4
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-11-22 11:39:23 -0800

    design is ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import twitterIcon from "./assets/twitter-icon.svg";
+import tumblrIcon from "./assets/tumblr-icon.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div id="quote-box" className="quote-box">
+        <div className="quote-text">
+          <i class="fa fa-quote-left"> </i>
+          <span id="text">
+            Life is what we make it, always has been, always will be.
+          </span>
+        </div>
+        <h3 id="author">- Author</h3>
+        <div className="card_footer">
+          <div className="social_media_buttons">
+            <a target="_blank" href="twitter.com/intent/tweet" id="tweet-quote">
+              {/* <i class="fa fa-twitter" aria-hidden="true"></i> */}
+              <img src={twitterIcon} alt="" />
+            </a>
+            <a href="twitter.com/intent/tweet" id="trumble-quote">
+              {/* <i class="fa fa-tumblr"></i> */}
+              <img src={tumblrIcon} alt="" />
+            </a>
+          </div>
+          <button id="new-quote">New Quote</button>
+        </div>
+      </div>
     </div>
   );
 }
